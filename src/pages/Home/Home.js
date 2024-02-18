1aaa98841e405a09a8eb40a9d0d8e7a70e76f56c
Kenneth Presedo <kennethpresedo@live.com>
2024-02-17 20:30:07 -0500

    home start

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import video from '/Users/kennethpresedo/Desktop/post-grad/eatery/src/assets/video.mp4'
+import restaurant from '/Users/kennethpresedo/Desktop/post-grad/eatery/src/assets/restaurant4.webp'
+import styles from './Home.module.scss'
 
 function Home() {
   return (
-    <div>Home
-
-
-      <video width="max-width" height="700" playing={true} controls >
-      <source src={video} type="video/mp4"/>
-     </video>
+    <>
+    <div className={styles.head}>
+      <img src={restaurant} alt='intro'/>
+      <div className={styles.left}>left</div>
+      <div className={styles.middle}>Branja<br /><span className={styles.middleB}>MIAMI</span></div>
+      <div className={styles.right}>right</div>
     </div>
+    </>
   )
 }
 
 export default Home
+
+{/* <video width="max-width" height="700" playing={true} controls >
+<source src={video} type="video/mp4"/>
+</video> */}
