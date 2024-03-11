2b1680460f7544348c12682b18fee470e326db0b
Kenneth Presedo <kennethpresedo@live.com>
2024-03-10 20:55:59 -0400

    ourstory

--- src/pages/OurStory/OurStory.js
+++ src/pages/OurStory/OurStory.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import styles from './OurStory.module.scss'
+import { motion } from 'framer-motion';
+
 
 const OurStory = () => {
   return (
-    <div>OurStory</div>
+    <>
+      <div className={styles.container}>
+      <motion.div
+    whileHover={{ scale: 1,  rotateZ: 90}}
+    whileTap={{
+      scale: 0.8,
+      rotate: -90,
+      borderRadius: "100%"
+    
+  }} className={styles.photoIn}>
+    <motion.div className={styles.inside}
+      whileHover={{
+        scale: 0,
+        transition: { duration: .2 },
+      }}
+    
+    ></motion.div>
+  </motion.div>
+
+      </div>
+    </>
   )
 }
 
